2cb437d087b0c9061975e8d6224d81f28a5b7b41
NickMallay <nickmallay@hotmail.com>
2025-11-23 13:16:56 -0500

    Add loading job postings from folder, and generating score reports for all files in the folder

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-## first pass will be super simple. 
-# Just take a pasted job posting, parse the text, 
-# keyword match, determine a score, return the score. 
-# Multiple job postings, llm intergreation for dynamic reccos, etc
-# will all be done as future iteration. 
-# The point today is to get this real, so its easier to work on. 
-
-##Get job text
-##format and list job text as list
-##compare job text list to personal profile / criteria
-##return score
+##First pass is complete. 
+# Next iterations will be to allow file access, mulutiple job postings, saving the output, and llm integration for a suitability summary
+import glob
 
 green_flags = [
     "qa",
@@ -41,7 +33,6 @@ green_flags = [
     "quality-driven",
     "contract",
     ] 
-
 red_flags = [
     "fast-paced",
     "fast paced",
@@ -70,8 +61,7 @@ red_flags = [
     "vibrant personality",
     "extroverted",
     ]
-    
-
+job_posting_folder_name = "job_posts"
 def get_green_flags(job_text, green_flags_list):
     green_flags_set = set() ## I want each keyword to count only once, so i am using a set rather than a list
     job_keyword_list = job_text.split(" ")
@@ -79,35 +69,53 @@ def get_green_flags(job_text, green_flags_list):
         if word in green_flags_list:
             green_flags_set.add(word)
     return green_flags_set
-
 def get_red_flags(job_text, red_flags_criteria):
     red_flag_matches = [] ## since each red_flag is checked against job_text only once, duplicates are impossible and so i've used a list here for simplicity
     for red_flag in red_flags_criteria:
         if red_flag in job_text:
             red_flag_matches.append(red_flag)
     return red_flag_matches
-
 def format_keywords(color, flag_list):
     formatted_keyword_list = ", ".join(list(flag_list))
     if len(formatted_keyword_list) >= 1:
         formatted_keyword_list += "."
-    print(f"{color} Flags: {formatted_keyword_list}")
-
-def print_score_summary(green_list, red_list):
+    return f"{color} Flags: {formatted_keyword_list}"
+def get_posting_report(name, green, red, score):
+    return f"Name: {name}\n{green}\n{red}\n{score}" #Pull together formatted parts of the report
+def get_score_summary(green_list, red_list):
     green_score = len(green_list) 
     red_score = len(red_list)
     total_score = green_score - red_score ## For now I am using a straight 1-1 scoring system. Later iterations will give weighting and even include "deal-breakers"
-    print(f"Green Flags: {green_score}\nRed Flags: {red_score}\nFinal Score: {total_score}")
+    return f"Green Flags: {green_score}\nRed Flags: {red_score}\nFinal Score: {total_score}\n"
+
+def get_job_posts(job_posting_folder_name):
+    job_posting_contents_dict = {}
+    job_post_file_list = glob.glob(f"{job_posting_folder_name}/*.txt")
+    for post in job_post_file_list:
+        with open(post) as p:
+            job_post_content = p.read()
+            job_posting_contents_dict[post] = job_post_content
+    print(job_posting_contents_dict)
+    return job_posting_contents_dict
+
 
 def main(): 
-    job_text = input("Please enter job text here:").lower()
-    green_flags_set = get_green_flags(job_text, green_flags)
-    red_flags_list = get_red_flags(job_text, red_flags)
+    job_posts = get_job_posts(job_posting_folder_name)
+    report_list = []
+    for job in job_posts:
+        job_name = jobc
+        job_content = job_posts[job]
+        
+        green_flags_set = get_green_flags(job_content, green_flags)
+        red_flags_list = get_red_flags(job_content, red_flags)
+        
+        green_flag_report = format_keywords("Green", green_flags_set)
+        red_flag_report = format_keywords("Red", red_flags_list)
+        score = get_score_summary(green_flags_set, red_flags_list)
 
-    print("\n\n----------RESULT----------")
-    format_keywords("Green", green_flags_set)
-    format_keywords("Red", red_flags_list)
-    print_score_summary(green_flags_set, red_flags_list)
-
+        job_report = get_posting_report(job_name, green_flag_report, red_flag_report, score)
+        report_list.append(job_report)
+    for report in report_list:
+        print(report)
 
 main()
